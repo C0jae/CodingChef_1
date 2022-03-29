069b48061171e6aa701112e548b8ecf55b8acfbc
Youngjae Choi <cdw0807@naver.com>
2022-03-30 01:45:18 +0900

    pocketmon info

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: '피카츄',
       home: Grade(),
     );
@@ -23,11 +24,11 @@ class Grade extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.amber[600],
+      backgroundColor: Colors.amber[700],
       appBar: AppBar(
         title: Text('피카츄'),
         centerTitle: true,
-        backgroundColor: Colors.amber[700],
+        backgroundColor: Colors.amber[600],
         elevation: 0.0,
       ),
       body:  Padding(
@@ -35,6 +36,20 @@ class Grade extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/pika1.jpeg'),
+                radius: 60.0,
+              ),
+            ),
+
+            Divider(
+              height: 60.0,
+              color: Colors.grey[850],
+              thickness: 0.5,
+              endIndent: 30.0,
+            ),
+
             Text('NAME',
             style: TextStyle(
               color: Colors.white,
@@ -54,6 +69,93 @@ class Grade extends StatelessWidget {
               fontWeight: FontWeight.bold,
             ),
             ),
+
+            SizedBox(
+              height: 30.0,
+            ),
+
+            Text('피카츄 POWER LEVEL',
+            style: TextStyle(
+              color: Colors.white,
+              letterSpacing: 2.0
+            ),
+            ),
+
+            SizedBox(
+              height: 10.0,
+            ),
+
+            Text('14',
+            style: TextStyle(
+              color: Colors.white,
+              letterSpacing: 2.0,
+              fontSize: 29.0,
+              fontWeight: FontWeight.bold,
+            ),
+            ),
+
+            SizedBox(
+              height: 30.0,
+            ),
+
+            Row(
+              children: [
+                Icon(Icons.check_circle_outline),
+
+                SizedBox(
+                  width: 10.0,
+                ),
+
+                Text('Lightning',
+                style: TextStyle(
+                  fontSize: 16.0,
+                  letterSpacing: 1.0
+                ),),
+              ],
+            ),
+
+            Row(
+              children: [
+                Icon(Icons.check_circle_outline),
+
+                SizedBox(
+                  width: 10.0,
+                ),
+
+                Text('Rush',
+                style: TextStyle(
+                  fontSize: 16.0,
+                  letterSpacing: 1.0
+                ),),
+              ],
+            ),
+
+            Row(
+              children: [
+                Icon(Icons.check_circle_outline),
+
+                SizedBox(
+                  width: 10.0,
+                ),
+
+                Text('Tail Attack',
+                style: TextStyle(
+                  fontSize: 16.0,
+                  letterSpacing: 1.0
+                ),),
+              ],
+            ),
+
+            SizedBox(
+              height: 30.0,
+            ),
+
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/pika2.jpeg'),
+                radius: 50.0,
+              ),
+            )
           ],
         ),
       ),
