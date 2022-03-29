ffd8b5d7e47ae6d869c293e7572f8eb4db406c77
Youngjae Choi <cdw0807@naver.com>
2022-03-29 23:36:24 +0900

    캐릭터 페이지 디자인

--- lib/main.dart
+++ lib/main.dart
@@ -2,42 +2,61 @@ import 'package:flutter/material.dart';
 
 
 void main() {
-  runApp(const myApp());
+  runApp(const MyApp());
 }
 
-class myApp extends StatelessWidget {
-  const myApp({ Key? key }) : super(key: key);
+class MyApp extends StatelessWidget {
+  const MyApp({ Key? key }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'First app',
-      theme: ThemeData(
-        primarySwatch: Colors.blue
-      ),
-      home: MyHomePage(),
+      title: '피카츄',
+      home: Grade(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({ Key? key }) : super(key: key);
+class Grade extends StatelessWidget {
+  const Grade({ Key? key }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.amber[600],
       appBar: AppBar(
-        title: Text('First app'),
+        title: Text('피카츄'),
+        centerTitle: true,
+        backgroundColor: Colors.amber[700],
+        elevation: 0.0,
       ),
-      body: Center(
+      body:  Padding(
+        padding: EdgeInsets.fromLTRB(30.0, 40.0, 0, 0),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text('Hello'),
-            Text('Hello'),
-            Text('Hello')
+            Text('NAME',
+            style: TextStyle(
+              color: Colors.white,
+              letterSpacing: 2.0
+            ),
+            ),
+
+            SizedBox(
+              height: 10.0,
+            ),
+
+            Text('피카츄',
+            style: TextStyle(
+              color: Colors.white,
+              letterSpacing: 2.0,
+              fontSize: 29.0,
+              fontWeight: FontWeight.bold,
+            ),
+            ),
           ],
         ),
-      )
+      ),
     );
   }
 }
