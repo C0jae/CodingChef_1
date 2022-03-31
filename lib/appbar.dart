bec4eb939357a33363a5bf135754b240cc7ae8a8
Youngjae Choi <cdw0807@naver.com>
2022-03-31 13:09:25 +0900

    Drawer ListTile

--- lib/appbar.dart
+++ lib/appbar.dart
@@ -48,55 +48,95 @@ class MyPage extends StatelessWidget {
         ],
       ),
       
+      drawer: Drawer(
+        child: ListView(
+          // 여백 없애기
+          padding: EdgeInsets.zero,
+          children: [
+            // 메뉴 리스트에 프로필 정보를 나타내기
+            UserAccountsDrawerHeader(
+              // 프로필 사진 설정, CircleAvatar : 원형의 이미지로 적용
+              currentAccountPicture: CircleAvatar(
+                // 적용 이미지의 경로 설정
+                backgroundImage: AssetImage('assets/pika1.jpeg'),
+              ),
+              // 주 프로필 사진 외의 사진 추가(여러개 첨부가 가능하므로 리스트[]를 가지게 된다.)
+              otherAccountsPictures: [
+                CircleAvatar(
+                  backgroundImage: AssetImage('assets/pika2.jpeg'),
+                )
+              ],
+              accountName: Text('피카츄'),
+              accountEmail: Text('Pika@naver.com'),
+              onDetailsPressed: () {
+                print('arrow is clicked');
+              },
+              decoration: BoxDecoration(
+                color: Colors.red[200],
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(40.0),
+                  bottomRight: Radius.circular(40.0),
+                )
+              ),
+            ),
+
+            ListTile(
+              leading: Icon(Icons.home,
+                color: Colors.grey[850]),
+              title: Text('home'),
+              onTap: () {
+                print('Home is clicked');
+              },
+              trailing: Icon(Icons.add),
+            ),
+
+            ListTile(
+              leading: Icon(Icons.settings,
+                color: Colors.grey[850]),
+              title: Text('Setting'),
+              onTap: () {
+                print('Setting is clicked');
+              },
+              trailing: Icon(Icons.add),
+            ),
+
+            ListTile(
+              leading: Icon(Icons.question_answer,
+                color: Colors.grey[850]),
+              title: Text('Q&A'),
+              onTap: () {
+                print('Q&A is clicked');
+              },
+              trailing: Icon(Icons.add),
+            ),
+          ],
+        ),
+      ),
+
       // drawer: Drawer(
       //   child: ListView(
       //     padding: EdgeInsets.zero,
       //     children: [
       //       UserAccountsDrawerHeader(
       //         currentAccountPicture: CircleAvatar(
-      //           backgroundImage: AssetImage('assets/pika2.jpeg'),
+      //           backgroundImage: AssetImage('assets/pika1.jpeg'),
       //         ),
       //         accountName: Text('피카츄'),
-      //         accountEmail: Text('Pika@naver.com'),
+      //         accountEmail: Text('Pika@gmail.com'),
       //         onDetailsPressed: () {
-      //           print('arrow is clicked');
+      //           print('arrow icon is clicked');
       //         },
       //         decoration: BoxDecoration(
       //           color: Colors.red[200],
       //           borderRadius: BorderRadius.only(
-      //             bottomLeft: Radius.circular(40.0),
-      //             bottomRight: Radius.circular(40.0),
+      //             bottomLeft: Radius.circular(30.0),
+      //             bottomRight: Radius.circular(30.0),
       //           )
-      //         ),
-      //       )
+      //         )
+      //       ),
       //     ],
       //   ),
       // ),
-
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: [
-            UserAccountsDrawerHeader(
-              currentAccountPicture: CircleAvatar(
-                backgroundImage: AssetImage('assets/pika1.jpeg'),
-              ),
-              accountName: Text('피카츄'),
-              accountEmail: Text('Pika@gmail.com'),
-              onDetailsPressed: () {
-                print('arrow icon is clicked');
-              },
-              decoration: BoxDecoration(
-                color: Colors.red[200],
-                borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(30.0),
-                  bottomRight: Radius.circular(30.0),
-                )
-              )
-            ),
-          ],
-        ),
-      ),
 
     );
   }
